e0ef0e45c18215fc578898d8d70ca7fa8744f1cb
Akin Erguler <65019791+AkinErguler@users.noreply.github.com>
2022-04-13 11:22:50 +0300

    Update KdvHesabi.java

--- KdvHesabi.java
+++ KdvHesabi.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class KdvHesabi {
     public static void main(String[] args) {
-        double volue,kdv,kdvliDeger,kdvoran=0.18;
+        double newVolue,volue,kdv,kdvliDeger,kdvoran1=0.18,kdvoran2=0.8;
         Scanner input=new Scanner(System.in);
         System.out.print("lütfen hesaplanacak para değerini giriniz ");
         volue=input.nextDouble();
-        kdv =(volue*kdvoran);
+        newvolue=(volue>0||(volue<1000)? kdvoran1:kdvoran2)
+        kdv =(volue*newvolue);
         kdvliDeger= volue+kdv;
         System.out.println("Girdiğiniz değer; "+volue+"TL");
         System.out.println("KDV DEĞERİ; "+kdv+"TL");
