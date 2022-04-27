6c5ae2aae4b3710d42029b3f911f4565a1f377fc
Akin Erguler <65019791+AkinErguler@users.noreply.github.com>
2022-04-27 11:49:47 +0300

    Add files via upload

---
+++ BasamakSayisiBulma.java
@@ -0,0 +1,20 @@
+package patikaodev;
+
+import java.util.Scanner;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class BasamakSayisiBulma {
+    public static void main(String[] args) {
+        int sayi,basamakTop=0;
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Lütfen sayıyı giriniz ");
+        sayi=inp.nextInt();
+        while (sayi!=0){
+        basamakTop+=sayi%10 ;
+        sayi/=10;
+        }
+        System.out.println(basamakTop);
+    }
+    }
+
+
